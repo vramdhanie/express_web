b011978d0b10f361002bdcc8a311343fae8bce55
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-12-05 09:58:50 -0400

    Add morgan and mongoose

--- server.js
+++ server.js
@@ -1,32 +1,59 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
 const app = express();
-const { PORT } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
+
+mongoose.Promise = global.Promise;
+app.use(morgan('common'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if(req.method === 'OPTIONS'){
+        return res.send(204);
+    }
+    next();
+});
+
+
 
 let server;
 
 const runServer = () => {
     return new Promise((resolve, reject) => {
-        server = app
-            .listen(PORT, () => {
-                console.log(`Server started on port ${PORT}`);
-                resolve();
+        mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+            if(err){
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Server started on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
             })
-            .on('error', err => {
-                reject(err);
-            });
     });
 }
 
 const closeServer = () => {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if(err){
-                return reject(err);
-            }
-            resolve();
+    return mongoose
+        .disconnect()
+        .then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if(err){
+                    return reject(err);
+                }
+                resolve();
+            });
         });
     });
 }
