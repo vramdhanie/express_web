ecce49502289421015b27b1373bb7a63af064584
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-12-06 16:26:20 -0400

    Add user model

---
+++ users/models.js
@@ -0,0 +1,47 @@
+"use strict"
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required:true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    firstName: {
+        type: String,
+        default: ""
+    },
+    lastName: {
+        type: String,
+        default: ""
+    },
+    email: {
+        type: String,
+        default: ""
+    }
+});
+
+UserSchema.methods.apiRepr = () => ({
+    username: this.username || '',
+    firstName: this.firstName || '',
+    lastName: this.lastName || '',
+    email: this.email || ''
+});
+
+UserSchema.methods.validatePassword = password => {
+    return bcrypt.compare(password, this.password);
+}
+
+UserSchema.statics.hashPassword = password => {
+    return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', UserSchema);
+module.exports = { User };
