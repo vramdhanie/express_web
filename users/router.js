7580893189eb3548213e60b7cd665df2edf211a8
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-12-14 07:57:28 -0400

    Add post user endpoint

--- users/router.js
+++ users/router.js
@@ -14,7 +14,39 @@ router.post('/', [check('username').exists(), check('password').exists()], (req,
     if(!errors.isEmpty()){
         return res.status(422).json({ errors: errors.mapped() });
     }
-    res.json({message: "success"});
+
+    const { username, password, firstName='', lastName='' } = req.body;
+
+    return User.find({username})
+        .count()
+        .then(count => {
+            if(count > 0){
+                return Promise.reject({
+                    code: 422,
+                    reason: 'ValidationError',
+                    message: 'Username already in use',
+                    location: 'username'
+                });
+            }
+            return User.hashPassword(password);
+        })
+        .then(hash => {
+            return User.create({
+                username,
+                password: hash,
+                firstName,
+                lastName
+            });
+        })
+        .then(user => {
+            return  res.status(201).json(user.serialize());
+        })
+        .catch(err => {
+            if(err.reason === 'ValidationError'){
+                return res.status(err.code).json(err);
+            }
+            return res.status(500).json({code: 500, message:'Internal server error'});
+        });
 });
 
 module.exports = { router };
