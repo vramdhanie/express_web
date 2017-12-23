118982b7d701da74d6d68b9bf21cfccd9536a83d
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-12-22 21:25:34 -0400

    Add pasport strategies for authentication

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/web';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
